d370031ad0baa56dcdbc65d45c7c75413e6efcde
Jason <bjason@gmail.com>
2014-02-03 22:22:56 -0800

    Fixing web version..

--- testosterone.js
+++ testosterone.js
@@ -11,13 +11,14 @@ if( this['document'] !=  null) {
 exports.count = 0;
 
 exports._markForWeb= function(text){
-    var body = document.getElementsByTagName("body");
+    var body = document.getElementsByTagName("body")[0];
     var element = document.createElement("div");
     element.innerHTML = text;
+    body.appendChild(element);
 };
 exports._markSuccess = function(testName){
     if (web) {
-        exports._markForWeb("success");
+        exports._markForWeb("Success");
     } else {
         console.log("\033[92m Success \033[92m \033[0m ");
     }
@@ -28,7 +29,11 @@ exports.log = function(log){
 };
 
 exports._markFailure= function(testName){
-    console.log("\033[31m Failure \033[91m \033[0m ");
+    if (web) {
+        exports._markForWeb("Failure");
+    } else {
+        console.log("\033[31m Failure \033[91m \033[0m ");
+    }
 };
 
 exports.assert = function(expected, got){
@@ -46,8 +51,5 @@ exports.assertEquals = function(expected, got){
 exports.assertTrue= function(got){
     exports.assert(true, got);
 }
-
-exports._markSuccess();
-exports._markFailure();
 
 testosterone = exports;
