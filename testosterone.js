fef39a31ed9f9b58ea931a319de20a46aaaa0e68
Jason <bjason@gmail.com>
2014-02-03 20:46:49 -0800

    Simple testing framework.

--- testosterone.js
+++ testosterone.js
@@ -1,5 +1,30 @@
-var testosterone = function(){
+var testosterone = { count: 0};
+
+testosterone._markSuccess = function(testName){
+    console.log("Success: ");
 };
 
+testosterone.log = function(log){
+    console.log(log):
+};
 
-testosterone.assert(f, "f");
+testosterone._markFailure= function(testName){
+    console.log("Failure: ");
+};
+
+testosterone.assert = function(expected, got){
+    if (expected === got) {
+        testosterone._markSuccess();
+    } else {
+        testosterone._markFailure();
+    }
+};
+
+testosterone.assertEquals = function(expected, got){
+    testosterone.assert(expected, got);
+}
+
+testosterone.assertTrue= function(got){
+    testosterone.assert(true, got);
+}
+
