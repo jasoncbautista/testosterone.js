5242d18418a32977f97b111c36308e4f0abb7e08
Jason <bjason@gmail.com>
2014-02-03 20:52:00 -0800

    Color coding example.

--- testosterone.js
+++ testosterone.js
@@ -1,15 +1,15 @@
 var testosterone = { count: 0};
 
 testosterone._markSuccess = function(testName){
-    console.log("Success: ");
+    console.log("Success");
 };
 
 testosterone.log = function(log){
-    console.log(log):
+    console.log(log);
 };
 
 testosterone._markFailure= function(testName){
-    console.log("Failure: ");
+    console.log("\033[31m Failure \033[91m \033[0m ");
 };
 
 testosterone.assert = function(expected, got){
@@ -28,3 +28,6 @@ testosterone.assertTrue= function(got){
     testosterone.assert(true, got);
 }
 
+
+testosterone._markSuccess();
+testosterone._markFailure();
