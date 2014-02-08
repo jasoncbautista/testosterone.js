2f523e97b2924f1ffe4c6988d29bb40116003bd3
Jason <bjason@gmail.com>
2014-02-08 13:12:26 -0800

    Testosterone testing.

--- testosterone.js
+++ testosterone.js
@@ -10,6 +10,11 @@ if( this['document'] !=  null) {
 
 exports.count = 0;
 
+/**
+ * Simple function to log to the DOM.
+ * @param {type} text,
+ * @return {Null}
+ */
 exports._markForWeb= function(text){
     var body = document.getElementsByTagName("body")[0];
     var element = document.createElement("div");
@@ -17,6 +22,11 @@ exports._markForWeb= function(text){
     body.appendChild(element);
     return element;
 };
+/**
+ * Logs  a test as successful
+ * @param {type} testName,
+ * @return {Null}
+ */
 exports._markSuccess = function(testName){
     if (web) {
         var element = exports._markForWeb("Success");
@@ -26,10 +36,20 @@ exports._markSuccess = function(testName){
     }
 };
 
+/**
+ * Shorthand for console.log
+ * @param {type} log,
+ * @return {Null}
+ */
 exports.log = function(log){
     console.log(log);
 };
 
+/**
+ * Logs a failed test
+ * @param {type} testName,
+ * @return {Null}
+ */
 exports._markFailure= function(testName){
     if (web) {
         var element = exports._markForWeb("Failure");
@@ -44,6 +64,13 @@ exports._markFailure= function(testName){
 exports.assertArraysEqual = function(expected, got){
     // ...
 };
+
+/**
+ * Asserts that parameter one equals parameter two
+ * @param {type} expected,
+ * @param {type} got,
+ * @return {Null}
+ */
 exports.assert = function(expected, got){
     if (expected === got) {
         exports._markSuccess();
@@ -52,10 +79,21 @@ exports.assert = function(expected, got){
     }
 };
 
+/**
+ * Asserts the two things are equal  (doesnt handle arrays well)
+ * @param {type} expected,
+ * @param {type} got,
+ * @return {Null}
+ */
 exports.assertEquals = function(expected, got){
     exports.assert(expected, got);
 }
 
+/**
+ * Asserts something is true.
+ * @param {type} got,
+ * @return {Null}
+ */
 exports.assertTrue= function(got){
     exports.assert(true, got);
 }
